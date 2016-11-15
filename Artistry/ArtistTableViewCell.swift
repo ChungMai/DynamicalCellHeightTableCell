d7260a2fda87f3aba2b4b3fe45a3bc4713fd94ba
ChungMai <macmini@MacMinis-Mac-mini.local>
2016-11-15 15:51:16 +0700

    For
    https://www.raywenderlich.com/129059/self-sizing-table-view-cells

---
+++ Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by MacMini on 11/15/16.
+//  Copyright © 2016 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+
+}
---
+++ Artistry/WorkTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WorkTableViewCell.swift
+//  Artistry
+//
+//  Created by MacMini on 11/15/16.
+//  Copyright © 2016 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell: UITableViewCell {
+  @IBOutlet weak var workImageView: UIImageView!
+  @IBOutlet weak var workTitleLabel: UILabel!
+  @IBOutlet weak var moreInfoTextView: UITextView!
+
+}
